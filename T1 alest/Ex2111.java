35962333a78f00dba0879ba10679a4d3644ab695
Arthur Accorsi <arthur.accorsi@edu.pucrs.br>
2023-09-08 20:26:31 -0300

    ex 2111 + ok 2111

--- T1 alest/Ex2111.java
+++ T1 alest/Ex2111.java
@@ -3,48 +3,52 @@ import java.util.Scanner;
 public class Ex2111 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         while (scanner.hasNext()) {
             int number = scanner.nextInt();
-            printSorobanRepresentation(number);
+            createSorobanRepresentation(number);
             System.out.println();
         }
     }
 
-    public static void printSorobanRepresentation(int number) {
-        int[] digits = new int[9]; // Array to store each digit
+    public static void createSorobanRepresentation(int number) {
+        int[] digits = distribuiDigitos(number);
 
-        // Extract each digit from the number
-        for (int i = 8; i >= 0; i--) {  
-            digits[i] = number % 10;
-            number /= 10;
-        }
-
-        // Print the top rows of stones (worth 5)
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (digits[j] >= 5) {
-                    System.out.print("1");
-                } else {
-                    System.out.print("0");
-                }
+        int[][] todasLinhas = new int[7][9];
+        todasLinhas[0] = new int[]{1,1,1,1,1,1,1,1,1};
+        todasLinhas[1] = new int[]{0,0,0,0,0,0,0,0,0};
+        todasLinhas[2] = new int[]{1,1,1,1,1,1,1,1,1};
+        todasLinhas[3] = new int[]{1,1,1,1,1,1,1,1,1};
+        todasLinhas[4] = new int[]{1,1,1,1,1,1,1,1,1};
+        todasLinhas[5] = new int[]{1,1,1,1,1,1,1,1,1};
+        todasLinhas[6] = new int[]{1,1,1,1,1,1,1,1,1};
+        
+        for (int i=0; i<9;i++){
+            if (digits[i] >= 5){
+                todasLinhas[0][i] = 0;
+                todasLinhas[1][i] = 1;
             }
-            System.out.println();
+            int numero = digits[i]%5;
+            todasLinhas[numero+2][i] = 0;
         }
-
-        // Print the separator line
-        System.out.println("---------");
-
-        // Print the bottom rows of stones (worth 1)
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (digits[j] % 5 > i) {
-                    System.out.print("1");
-                } else {
-                    System.out.print("0");
-                }
+        printSorobov(todasLinhas);
+    }
+    private static int[] distribuiDigitos(int numero){
+            int[] digits = new int[9];
+        for (int i = 8; i >= 0; i--) {
+            digits[i] = numero % 10;
+            numero /= 10;
+        }
+        return digits;
+    }
+    private static void printSorobov(int[][] todasLinhas){
+        for(int i = 0; i<7; i++){
+            for (int j=0; j<9; j++){
+                System.out.print(todasLinhas[i][j]);                
             }
-            System.out.println();
+            if (i==1){
+                System.out.print("\n---------");
+            }    
+        System.out.println();
         }
     }
 }
